822c717603db69eea97e0ee32625f5e760bbaac9
Trần Đinh Ngôn <tdngon01@gmail.com>
2026-07-05 21:25:24 +0700

    bo sung lan 3

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -2,12 +2,23 @@
 import React from 'react';
 
 class MyComponent extends React.Component {
+
+    handleClick(event) {
+        console.log(event.target)
+    }
+
+    handleOnMoveOver(event) {
+        console.log(event.pageX)
+    }
+
     //JSX
     render() {
         return (
             <div>
                 <h1>Hello React Class Component</h1>
-                {Math.random()}
+                <button onClick={this.handleClick}>One Click</button>
+                <br />
+                <button onClick={this.handleOnMoveOver}>Two Click</button>
             </div>
         );
     }
